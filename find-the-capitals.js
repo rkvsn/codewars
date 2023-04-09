0cdc84f81b714d95e74ec09eeeefd124fc9b542f
Richard Klein <richi31@gmail.com>
2023-04-09 13:44:47 -0400

    find the capitals

---
+++ find-the-capitals.js
@@ -0,0 +1,18 @@
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+
+
+var capitals = function (word) {
+  let index = [];
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      index.push(i);
+    }
+  }
+  return index;
+};
