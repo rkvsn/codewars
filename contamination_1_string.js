a81f94f0ed95293703a5006654c661671a21c7c0
Richard Klein <richi31@gmail.com>
2023-04-13 09:38:54 -0400

    contamination 1 string

---
+++ contamination_1_string.js
@@ -0,0 +1,25 @@
+// An AI has infected a text with a character!!
+
+// This text is now fully mutated to this character.
+
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+
+// Note: The character is a string of length 1 or an empty string.
+
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+
+
+function contamination(text, char) {
+  let str = "";
+  if (text == "" || char == "") {
+    return "";
+  }
+  for (let i = 0; i < text.length; i++) {
+    str += char;
+  }
+  return str;
+}
