ce1a985978ece02634bcc002f92c31fcf2e14ebd
Richard Klein <richi31@gmail.com>
2023-04-18 15:44:49 -0400

    factorial

---
+++ factorial.js
@@ -0,0 +1,12 @@
+// Your task is to write function factorial.
+
+// https://en.wikipedia.org/wiki/Factorial
+
+
+function factorial(n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
