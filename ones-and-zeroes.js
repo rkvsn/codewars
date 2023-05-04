7285716dca46b77332414be66e36008a5ea3db39
Richard Klein <richi31@gmail.com>
2023-05-04 18:36:46 -0400

    ones and zeroes

---
+++ ones-and-zeroes.js
@@ -0,0 +1,30 @@
+// DESCRIPTION:
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+//PARAMETERS: an array of ones and zeros, could be varying length 
+//RETURNS: an integer representation of the binary number given
+//EXAMPLES: Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+//PSUEDOCODE: tkae array and turn it into a string and then conver tto binary using 
+//string and return integer equivalent
+
+
+const binaryArrayToNumber = (arr) => {
+  return parseInt(arr.join(''), 2)
+};
